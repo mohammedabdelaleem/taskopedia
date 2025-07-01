0dce1507e9eb880bdd545fe2defb25eecea56c52
mohammedabdelaleem <mohammedabdelaleem20@gmail.com>
2025-07-01 15:04:53 +0300

    React Revision :Jsx - jsx constraints ,  Components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+function MainHeader()
+{
+  return (
+  <header>
+      <h1>React Course</h1>
+  </header>
+  )
+}
+
+function SubHeader()
+{
+  return (
+      <h3>This Will Be An Exciting Course</h3>
+  )
+}
+
+function Header()
+{
+  return (
+    <header>
+      <MainHeader/>
+      <SubHeader/>
+    </header>
+  )
+}
+
+
+function MainComponent()
+{
+  return (
+  <main>
+      <p>In This Course We Will Learning React By Building ReactOpedia!!</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to Walmart</li>
+      </ul>
+  </main>
+  )
+}
+
+function Footer(){
+  return (
+    <footer>
+      <h2>Happy Coding</h2>
+    </footer>
+  )
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <div>
+    <Header/>
+    <MainComponent/>
+    <Footer/>
+  </div>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
